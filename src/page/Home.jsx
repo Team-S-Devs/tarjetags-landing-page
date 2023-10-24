9ceb694c09bd1908ee6fcb0267bcc6fbcca15a66
just-fabian <fabian130404@gmail.com>
2023-10-24 11:41:02 -0400

    feat: is user logged

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,24 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import React from 'react'
+import { auth } from '../utils/firebase-config';
 
 const Home = () => {
+    const [isUserLogged, setIsUserLogged] = useState(false);
+
+    useEffect(() => {
+        const handleAuthStateChange = () => {
+          onAuthStateChanged(auth, (user) => {
+            if (user) {
+              setIsUserLogged(true);
+            } else {
+              setIsUserLogged(false);
+            }
+          });
+        };
+    
+        handleAuthStateChange();
+      }, []);
+    
   return (
     <div>Home</div>
   )
