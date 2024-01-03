4898272c880627b81084569f275fd945157ea590
gandel.dev <gandel.dev@gmail.com>
2024-01-02 21:18:32 -0400

    Footer finish

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import '../../styles/Footer.css'
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import footerMenuSubelementsJSON from '../../utils/FooterMenuSubelements.json'
+import { FooterMenuSubelement } from './FooterMenuSubelement';
+
+export const Footer = () => {
+
+	const iconsColor = '#ffffff'
+	const iconsSize = 32;
+
+	return (
+		<footer className='footer'>
+			<div className="footer-element-container footer-description">
+				<h2>LOGO</h2>
+				<p className='description-tarjetags-footer'>Irure Lorem proident dolore consequat nisi non officia nostrud consectetur fugiat eiusmod ipsum reprehenderit.</p>
+			</div>
+			<div className="footer-element-container footer-menu">
+				{
+					footerMenuSubelementsJSON.map((element) => {
+						return <FooterMenuSubelement title={element['name-section']} subelements={element.subelements} key={element.id} />
+					})
+				}
+			</div>
+			<div className="footer-element-container social-medias">
+				<h4 className='subtitle-footer'>Síguenos</h4>
+				<div className='social-medias-section'>
+					<a className='social-media-link' href='/'>
+						<FaFacebook color={iconsColor} size={iconsSize} />
+					</a>
+					<a className='social-media-link' href='/'>
+						<FaInstagram color={iconsColor} size={iconsSize} />
+					</a>
+					<a className='social-media-link' href='/'>
+						<FaTiktok color={iconsColor} size={iconsSize} />
+					</a>
+				</div>
+			</div>
+		</footer>
+	)
+}
