69f48c5626433d406d5cb71f10aa4bea91df5321
gandel.dev <gandel.dev@gmail.com>
2024-01-09 18:52:02 -0400

    Fix main features section

--- src/components/Home/MainFunction.jsx
+++ src/components/Home/MainFunction.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import '../../styles/Home/MainFunction.css'
 import PropTypes from 'prop-types'
 
-export const MainFunction = ({ name, description, image, isLeft }) => {
+export const MainFunction = ({ name, description, image, isLeft, currentPosition, id }) => {
+
+	const [position, setPosition] = useState("");
+
+    useEffect(() => {
+        if (id < currentPosition) {
+            setPosition("left-position")
+        } else if (id > currentPosition) {
+            setPosition("right-position")
+        } else {
+            setPosition("center-position")
+        }
+    }, [id, currentPosition])
+
 	return (
-		<div className="main-function-container">
+		<div className={`main-function-container ${position}`}>
 			<div className={`main-function-details ${isLeft ? "left" : "right"}`}>
 				<h2 className="main-function-name">
 					{
@@ -27,5 +41,7 @@ MainFunction.propTypes = {
 	name: PropTypes.string.isRequired,
 	description: PropTypes.string.isRequired,
 	image: PropTypes.string.isRequired,
-	isLeft: PropTypes.bool.isRequired
+	isLeft: PropTypes.bool.isRequired,
+	currentPosition: PropTypes.number.isRequired,
+	id: PropTypes.number.isRequired
 }
