36db00098cf545c23721a311daa075e2304525c0
VillalobosGarciaGaby <gabriela.garcia.13355797@usalesiana.edu.bo>
2024-01-22 00:05:27 -0400

    text

--- src/components/Home/fun.jsx
+++ src/components/Home/fun.jsx
@@ -3,20 +3,67 @@ import ParallaxCard from "../../components/Home/MainFunctionalities";
 import "../../styles/Home/fun.css";
 
 const FuncionalityPage = () => {
-  return (
-    <div className="funcionality-page">
-      {/* Primera sección con ParallaxCard */}
-      <div className="parallax-section">
-        <ParallaxCard />
-      </div>
+    return (
+        <div className="funcionality-page">
+            <div className='funcionality-conteiner'>
+                <div className="parallax-section">
+                    <ParallaxCard />
+                </div>
+                <div className="title-section">
+                    <h2>Funcionalidades Principales</h2>
+                    <div className="fun-section">
+                        <div className='icon'>
+                            <div className='icon-center'>
+                                <img src="https://i.ibb.co/n80LSTF/trending-up.png" alt="icon de la imagen" />
+                                <img src="https://i.ibb.co/6FhS3Wd/Ellipse-62-removebg-preview.png" alt="Descripción de la imagen" />
+                            </div>
+                        </div>
+                        <div className='text-section'>
+                            <h4 className='title-text'>Potencia tus servicios </h4>
+                            <div className='subtext-text'>Añade y personaliza una variedad de servicios para destacar tu oferta de manera atractiva.</div>
+                        </div>
+                    </div>
+                    <div className="fun-section">
+                        <div className='icon'>
+                            <div className='icon-center'>
+                                <img src="https://i.ibb.co/n80LSTF/trending-up.png" alt="icon de la imagen" />
+                                <img src="https://i.ibb.co/6FhS3Wd/Ellipse-62-removebg-preview.png" alt="Descripción de la imagen" />
+                            </div>
+                        </div>
+                        <div className='text-section'>
+                            <h4 className='title-text'>Potencia tus servicios </h4>
+                            <div className='subtext-text'>Añade y personaliza una variedad de servicios para destacar tu oferta de manera atractiva.</div>
+                        </div>
+                    </div>
+                    <div className="fun-section">
+                        <div className='icon'>
+                            <div className='icon-center'>
+                                <img src="https://i.ibb.co/n80LSTF/trending-up.png" alt="icon de la imagen" />
+                                <img src="https://i.ibb.co/6FhS3Wd/Ellipse-62-removebg-preview.png" alt="Descripción de la imagen" />
+                            </div>
+                        </div>
+                        <div className='text-section'>
+                            <h4 className='title-text'>Potencia tus servicios </h4>
+                            <div className='subtext-text'>Añade y personaliza una variedad de servicios para destacar tu oferta de manera atractiva.</div>
+                        </div>
+                    </div>
+                    <div className="fun-section">
+                        <div className='icon'>
+                            <div className='icon-center'>
+                                <img src="https://i.ibb.co/n80LSTF/trending-up.png" alt="icon de la imagen" />
+                                <img src="https://i.ibb.co/6FhS3Wd/Ellipse-62-removebg-preview.png" alt="Descripción de la imagen" />
+                            </div>
+                        </div>
+                        <div className='text-section'>
+                            <h4 className='title-text'>Potencia tus servicios </h4>
+                            <div className='subtext-text'>Añade y personaliza una variedad de servicios para destacar tu oferta de manera atractiva.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-      {/* Segunda sección con el título "Funcionalidades Principales" */}
-      <div className="title-section">
-        <h2>Funcionalidades Principales</h2>
-        {/* Agrega más contenido si es necesario */}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default FuncionalityPage;
