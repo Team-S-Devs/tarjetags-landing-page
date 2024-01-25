5af7492e0f0e53b5986110a792d2ab21c42d0dd3
Fernando Mauricio Mamani Navarro <fernando.mamani.13389201@usalesiana.edu.bo>
2024-01-25 03:54:47 -0400

    feat: carousel

--- src/components/Home/BenefitsFunction.jsx
+++ src/components/Home/BenefitsFunction.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
-import { useSpring, animated, config } from 'react-spring';
-import ParallaxCard from "./MainFunctionalities";
+
 import StartButton from "./StartButton";
-import { svgCode6 } from "../../utils/svgData";
 import "../../styles/Home/BenefitsFunction.css";
 import commentsData from '../../utils/Comments.json';
-import { useRef } from 'react';
-import { useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import Flickity from 'flickity';
 import 'flickity/css/flickity.css';
-
-const createSvgImage = (svgCode) => `data:image/svg+xml;base64,${btoa(svgCode)}`;
-const imgSrc4 = createSvgImage(svgCode6);
+import ParallaxTilt from 'react-parallax-tilt';
+import pain from '../../assets/mancha.svg';
 
 const BenefitsFunction = () => {
-    const [index, setIndex] = React.useState(0);
-
     
-   
     const carouselRef = useRef(null);
     const flickityInstance = useRef(null);
    
-
     useEffect(() => {
    
         flickityInstance.current = new Flickity(carouselRef.current, {
           
-          autoplay: true,
+          autoPlay: 3000,
           prevNextButtons: true,
           pageDots: false,
           contain: true,
-             wrapAround: true,
+        wrapAround: true,
         });
         return () => {
             if(flickityInstance.current){
@@ -39,33 +31,45 @@ const BenefitsFunction = () => {
         }
     }, []);
 
-
-
-
-
     return (
         <div className="benefit-section-benefit">
             <div className="container body-benefits">
                 <div className="title-section-benefit">
-                    <h3>Beneficios</h3>
+                    
                 </div>
                 <div className='funcionality-conteiner-benefit'>
                     <div className='container-carousel-home-benefit'>
                         <div ref={carouselRef} className="carousel-home">
                             {commentsData.map((comment, i) => (
-                                
                                 <div key={i} className="carousel-cell-home">
                                     <div className="comment-benefit">
-                                        <StartButton white={true}/>
-                                        <h5>{comment.title}</h5>
+                                        <h4 className='title-benefit-carousel'>{comment.title}</h4>
                                         <p>{comment.text}</p>
-                                        =
                                     </div>
                                 </div>
                             ))}
+                            
+                        </div>
+                        <div className="start-button-benefit">
+                            <StartButton white={true} />
                         </div>
                     </div>
                     <div className='container-card-benefit'>
+                        <img src={pain} alt="pain" className="back-benefit" />
+                        <ParallaxTilt
+                            scale={1}
+                            transitionSpeed={1500}
+                            tiltMaxAngleX={15}
+                            tiltMaxAngleY={15}
+                        >
+                            <div className="image-container-benefit">
+                                <img
+                                    src="https://i.postimg.cc/V657vNgn/Preview-tarjeta.png"
+                                    alt="Centrado con perspectiva"
+                                    className="img-benefit"
+                                />
+                            </div>
+                        </ParallaxTilt>
                     </div>
                 </div>
             </div>
