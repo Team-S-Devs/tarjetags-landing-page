1d139158b95f432683630a758ac5546d8a603207
gabygabylagaby <gabrielagv7656@gmail.com>
2024-03-18 13:07:40 -0400

    planes tab completado

--- src/page/Plans.jsx
+++ src/page/Plans.jsx
@@ -18,39 +18,59 @@ const Plans = () => {
   return (
     <>
       <BannerTittle />
-      <Box sx={{ width: "100%", margin: "10px 0" }}>
+      <Box sx={{ width: "100%" }}>
         <TabContext value={value}>
           <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <div style={{ display: "flex", width:'100%', justifyContent:'center'}}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "-48px",
+                height: "auto",
+              }}
+            >
               <TabList
                 onChange={handleChange}
-                textColor="secondary"
-                indicatorColor="secondary"
+                textColor="black"
+                indicatorColor="none"
                 sx={{
                   display: "flex",
                   flexDirection: "row",
                   gap: "10px",
-                  marginBottom: "10px",
+                  backgroundColor: "transparent",
                 }}
               >
                 <Tab
                   value="plans"
                   label="Planes Standard"
-                  sx={{ flex: 1, width: "100%" }}
+                  sx={{
+                    flex: 1,
+                    width: "300px",
+                    backgroundColor:"white" ,
+                    "&.Mui-selected": {
+                      backgroundColor:  "#EDF0F7",
+                      fontWeight: 'bold',
+                    },
+                  }}
                 />
                 <Tab
                   value="plansPartner"
                   label="Planes de Socios"
-                  sx={{ flex: 1, width: "100%" }}
+                  sx={{ flex: 1, width: "300px", backgroundColor:"white" ,
+                  "&.Mui-selected": {
+                    backgroundColor:  "#EDF0F7",
+                    fontWeight: 'bold',
+                  },
+                }}
                 />
               </TabList>
             </div>
 
-            <TabPanel value="plans">
+            <TabPanel value="plans" sx={{ backgroundColor: "#EDF0F7", width: "100%"}}>
               <PricesSection />
               <TableComparePlans />
             </TabPanel>
-            <TabPanel value="plansPartner">
+            <TabPanel value="plansPartner" sx={{ backgroundColor: "#EDF0F7", width: "100%" }}>
               <PricesSection />
             </TabPanel>
           </Box>
