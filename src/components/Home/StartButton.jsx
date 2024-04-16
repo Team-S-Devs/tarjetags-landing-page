001a3966445565721b93d67aa14cc03b036d81ac
gabygabyLagaby <gabrielagv7656@gmail.com>
2024-04-15 23:27:06 -0400

    Refactor: imagen FaQ

--- src/components/Home/StartButton.jsx
+++ src/components/Home/StartButton.jsx
@@ -1,42 +1,43 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { linkHome} from "../../utils/Links.js";
+import { linkHome } from "../../utils/Links.js";
 import { useState, useEffect } from "react";
 
-const StartButton = ({white}) => {
+const StartButton = ({ white }) => {
+  /* TODO: Aqui ira la logica de obtener el booleano de logueado o no */
+  const [isLogged, setIsLogged] = useState(false);
+  const [message, setMessage] = useState("");
 
-    /* Aqui ira la logica de obtener el booleano de logueado o no */
-    const [isLogged, setIsLogged] = useState(false);
-
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        if (isLogged) {
-            setMessage("Mi dashboard");
-        } else {
-            setMessage("Comenzar");
-        }
-    }, [isLogged]);
-
-    const navigateToLogin = () => {
-        window.location.href = linkHome;
+  useEffect(() => {
+    if (isLogged) {
+      setMessage("Mi dashboard");
+    } else {
+      setMessage("Comenzar");
     }
-    return (
-       <Button variant="contained" onClick={navigateToLogin}
-        sx={{
-            backgroundColor: white? ("white") : "#662483", 
-            borderRadius:"25px",
-            width:"200px",
-            height:"50px",
-            color: white? ("#662483") : "white",
-            fontSize:"20px",
-            '&:hover': {
-                backgroundColor: white? "white": "#662483",
-                }
-            }} >
-              {message}
-         </Button>
-    );
-}
+  }, [isLogged]);
+
+  const navigateToLogin = () => {
+    window.location.href = linkHome;
+  };
+  return (
+    <Button
+      variant="contained"
+      onClick={navigateToLogin}
+      sx={{
+        backgroundColor: white ? "white" : "#662483",
+        borderRadius: "25px",
+        width: "200px",
+        height: "50px",
+        color: white ? "#662483" : "white",
+        fontSize: "20px",
+        "&:hover": {
+          backgroundColor: white ? "white" : "#662483",
+        },
+      }}
+    >
+      {message}
+    </Button>
+  );
+};
 
 export default StartButton;
